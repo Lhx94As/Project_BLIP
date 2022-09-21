32860896801ba02009b597fbd5692783e004b62a
vicchuayh <40730036+vicchuayh@users.noreply.github.com>
2022-09-21 14:54:18 +0800

    Update download_dependency_linux.py
    Removed sudo for conda installing packages as it causes error and packages cannot be loaded successfully.

--- download_dependency_linux.py
+++ download_dependency_linux.py
@@ -2,22 +2,22 @@ import subprocess
 
 
 print("Install torch")
-subprocess.call("sudo conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch", shell = True)
+subprocess.call("conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch", shell = True)
 
 print("Install librosa")
-subprocess.call("sudo conda install -c conda-forge librosa", shell = True)
+subprocess.call("conda install -c conda-forge librosa", shell = True)
 
 print("Install soundfile")
-subprocess.call("sudo conda install -c conda-forge pysoundfile", shell = True)
+subprocess.call("conda install -c conda-forge pysoundfile", shell = True)
 
 print("Install sklearn")
-subprocess.call("sudo conda install -c anaconda scikit-learn", shell = True)
+subprocess.call("conda install -c anaconda scikit-learn", shell = True)
 
 print("Install tqdm")
-subprocess.call("sudo conda install -c conda-forge tqdm", shell = True)
+subprocess.call("conda install -c conda-forge tqdm", shell = True)
 
 print("Install pydub")
-subprocess.call("sudo conda install -c conda-forge pydub", shell = True)
+subprocess.call("conda install -c conda-forge pydub", shell = True)
 
 print("Install sox")
 subprocess.call("sudo apt-get -y install sox", shell = True)
@@ -26,7 +26,7 @@ print("Install s3prl")
 subprocess.call("pip install s3prl", shell = True)
 
 print("Install argparse")
-subprocess.call("sudo conda install -c conda-forge argparse", shell = True)
+subprocess.call("conda install -c conda-forge argparse", shell = True)
 
 print("Install collection")
-subprocess.call("sudo conda install -c lightsource2-tag collection", shell = True)
+subprocess.call("conda install -c lightsource2-tag collection", shell = True)
